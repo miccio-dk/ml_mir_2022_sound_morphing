data normalization, more plots, notebooks, model architecture changes, various fixes, etc

src/dataset.py
@@ -38,9 +38,13 @@ class NsynthDataset(Dataset):
         if self.transform:
             sample = self.transform(sample)
         if self.label == 'onehot':
-            label = self.onehot[idx]
-        else:
+            label = self.onehot[idx].float()
+        elif self.label == 'full':
             label = item.drop(['qualities_str', 'qualities']).to_dict()
+        elif self.label == 'both':
+            label = item.drop(['qualities_str', 'qualities']).to_dict(), self.onehot[idx]
+        else:
+            None
         return sample, label
 
     def load_data(self):
@@ -77,3 +81,11 @@ class NsynthDataset(Dataset):
 
     def get_n_classes(self):
         return len(self.df['instrument'].unique())
+
+    def get_average_datapoint(self):
+        all_data = []
+        for i in tqdm(range(self.__len__())):
+            sample, _ = self.__getitem__(i)
+            all_data.append(sample)
+        all_data = torch.stack(all_data)
+        return all_data.mean(0), all_data.std(0)

src/loss.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 
 
 class VaeLoss(nn.Module):
-    def __init__(self, rec_weight, kld_weight):
+    def __init__(self, rec_weight, kld_weight, **kwargs):
         super(VaeLoss, self).__init__()
         self.rec_weight = rec_weight
         self.kld_weight = kld_weight
@@ -17,17 +17,19 @@ class VaeLoss(nn.Module):
         return loss, rec, kld
 
 
-class VaeLossExtended(VaeLoss):
-    def __init__(self, rec_weight, kld_weight, ce_weights, lspace_size, n_labels):
-        super(VaeLossExtended, self).__init__(rec_weight, kld_weight)
-        self.ce_weights = ce_weights
-        self.fc = nn.Linear(lspace_size, n_labels)
+class VaeLossClasses(VaeLoss):
+    def __init__(self, rec_weight, kld_weight, ce_weight, lspace_size, n_classes, **kwargs):
+        super(VaeLossClasses, self).__init__(rec_weight, kld_weight)
+        self.ce_weight = ce_weight
+        self.fc_classes = nn.Linear(lspace_size, n_classes)
 
     def forward(self, x_true, x_pred, mean, log_var, z, label_true):
-        _loss, rec, kld = super(VaeLossExtended, self).forward(x_true, x_pred, mean, log_var)
-        logits = self.fc(z)
-        ce = F.cross_entropy(logits, label_true, reduction='mean')
-        loss = _loss + ce * self.ce_weights
+        _loss, rec, kld = super(VaeLossClasses, self).forward(x_true, x_pred, mean, log_var, z, label_true)
+        if label_true is not None and len(label_true) == x_true.shape[0]:
+            logits = self.fc_classes(z)
+            ce = F.cross_entropy(logits, label_true, reduction='mean')
+        else:
+            ce = torch.Tensor([0.0]).to(x_true.device)
+        loss = _loss + ce * self.ce_weight
         return loss, rec, kld, ce
-
 

src/model.py
@@ -76,13 +76,13 @@ class VaeModel(nn.Module):
         # DECODER - Using PixelShuffle Layers
 
         self.convTrans5 = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=256, out_channels=1024, kernel_size=(3, 3), stride=(2, 2)),
-            nn.BatchNorm2d(1024, momentum=0.01),
+            nn.ConvTranspose2d(in_channels=64, out_channels=128, kernel_size=(3, 3), stride=(2, 2)),
+            nn.BatchNorm2d(128, momentum=0.01),
             nn.ReLU(inplace=True),
         )
 
         self.convTrans6 = nn.Sequential(
-            nn.PixelShuffle(upscale_factor=2),
+            nn.ConvTranspose2d(in_channels=128, out_channels=256, kernel_size=(3, 3), stride=(2, 2)),
             nn.BatchNorm2d(256, momentum=0.01),
             nn.ReLU(inplace=True),
         )
@@ -96,7 +96,7 @@ class VaeModel(nn.Module):
         self.convTrans8 = nn.Sequential(
             nn.PixelShuffle(upscale_factor=2),
             nn.BatchNorm2d(16, momentum=0.01),
-            nn.ReLU(inplace=True),
+            # nn.ReLU(inplace=True),
         )
 
         self.convTrans9 = nn.Sequential(
@@ -106,9 +106,9 @@ class VaeModel(nn.Module):
         )
 
         self.convTrans10 = nn.Sequential(
-            nn.PixelShuffle(upscale_factor=2),
+            nn.ConvTranspose2d(in_channels=4, out_channels=1, kernel_size=(1, 1)),
             nn.BatchNorm2d(1, momentum=0.01),
-            # nn.ReLU()
+            # nn.ReLU(inplace=True),
         )
 
     def encode(self, x):
@@ -133,7 +133,7 @@ class VaeModel(nn.Module):
 
     def decode(self, z):
         x = self.relu(self.fc_bn4(self.fc4(z)))
-        x = self.relu(self.fc_bn5(self.fc5(x))).view(x.shape[0], -1, 4, 4)
+        x = self.relu(self.fc_bn5(self.fc5(x))).view(x.shape[0], -1, 2, 7)
         x = self.convTrans5(x)
         x = self.convTrans6(x)
         x = self.convTrans7(x)

src/train.py
@@ -5,11 +5,11 @@ import torch
 import pandas as pd
 
 from tqdm import tqdm
-from utils import (Bunch, set_seed, get_preprocessing, get_dataloader, chart_dependencies, 
-        get_now, store_checkpoint, plot_reconstructions, plot_latentspace)
+from utils import (Bunch, set_seed, get_preprocessing, get_dataloader, chart_dependencies, kld_scheduler,
+        get_now, store_checkpoint, plot_reconstructions, plot_latentspace, plot_latentspace_pca)
 from dataset import NsynthDataset
 from model import VaeModel
-from loss import VaeLoss
+from loss import VaeLoss, VaeLossClasses
 
 
 def train(cfg):
@@ -21,9 +21,11 @@ def train(cfg):
         torch.set_num_threads(cfg.num_workers)
 
     print('# Creating preprocessing, datasets, dataloaders')
+    cfg.data_mean = torch.load('data_mean.pt')
+    cfg.data_std = torch.load('data_std.pt')
     preproc_train = get_preprocessing(n_mels=cfg.n_mels, data_mean=cfg.data_mean, data_std=cfg.data_std, log_transform=True, train=True)
     preproc_valid = get_preprocessing(n_mels=cfg.n_mels, data_mean=cfg.data_mean, data_std=cfg.data_std, log_transform=True, train=False)
-    ds_train = NsynthDataset('/home/rmiccini/stanford_mir2/data/nsynth-train', sr=cfg.sr, duration=cfg.duration, pitches=[60], transform=preproc_train, label='onehot')
+    ds_train = NsynthDataset('/home/rmiccini/stanford_mir2/data/nsynth-train', sr=cfg.sr, duration=cfg.duration, pitches=[60], transform=preproc_train, label='both')
     ds_valid = NsynthDataset('/home/rmiccini/stanford_mir2/data/nsynth-valid', sr=cfg.sr, duration=cfg.duration, pitches=[60], transform=preproc_valid, label='full')
     dl_train = get_dataloader(ds_train, batch_size=cfg.batch_size, num_workers=cfg.num_workers, seed=cfg.seed, shuffle=True)
     dl_valid = get_dataloader(ds_valid, batch_size=cfg.batch_size, num_workers=cfg.num_workers, seed=cfg.seed, shuffle=False)
@@ -34,7 +36,11 @@ def train(cfg):
     print(f'Validation: {len(ds_valid)} datapoints, {len(dl_valid)} batches')
 
     print('# Creating loss, model, optimizer')
-    loss = VaeLoss(rec_weight=cfg.rec_weight, kld_weight=cfg.kld_weight)
+    LossClass = {
+        'vae': VaeLoss,
+        'classes': VaeLossClasses,
+    }[cfg.loss_type]
+    loss = LossClass(rec_weight=cfg.rec_weight, kld_weight=cfg.kld_weight, ce_weight=cfg.ce_weight, lspace_size=cfg.lspace_size, n_classes=n_classes)
     model = VaeModel(loss=loss, fc_hidden1=cfg.fc_hidden1, fc_hidden2=cfg.fc_hidden2, fc_hidden3=cfg.fc_hidden3, lspace_size=cfg.lspace_size)
     optimizer = torch.optim.Adam(model.parameters(), lr=cfg.start_lr)
     chart_dependencies(model, input_shape=(cfg.batch_size, 1, cfg.n_mels, n_timeframes))
@@ -57,6 +63,8 @@ def train(cfg):
         optimizer=optimizer,
         dl_train=dl_train,
         dl_valid=dl_valid,
+        kld_weight=cfg.kld_weight,
+        kld_exp=cfg.kld_exp,
         val_every=cfg.val_every,
         device=device,
         wandb_run=wandb_run,
@@ -64,7 +72,7 @@ def train(cfg):
 
     print('# Starting testing')
     ds_test = NsynthDataset('/home/rmiccini/stanford_mir2/data/nsynth-test', sr=cfg.sr, duration=cfg.duration, pitches=[60], transform=preproc_valid, label='full')
-    dl_test = get_dataloader(ds_test, batch_size=cfg.batch_size, num_workers=cfg.num_workers, seed=cfg.seed, shuffle=False)
+    dl_test = get_dataloader(ds_test, batch_size=cfg.batch_size, num_workers=cfg.num_workers, seed=cfg.seed, shuffle=False, drop_last=False)
     print(f'Testing: {len(ds_test)} datapoints, {len(dl_test)} batches')
     test(
         model=model,
@@ -75,24 +83,27 @@ def train(cfg):
     wandb_run.finish()
 
 
-def training_loop(epochs, model, optimizer, dl_train, dl_valid, val_every, device='cpu', wandb_run=None):
+def training_loop(epochs, model, optimizer, dl_train, dl_valid, val_every, kld_weight, kld_exp, device='cpu', wandb_run=None):
     model = model.to(device)
     for epoch in range(1, epochs + 1):
-        train_one_epoch(model, dl_train, optimizer, current_epoch=epoch, total_epochs=epochs, device=device, wandb_run=wandb_run)
-        if epoch % val_every == 0 or epoch == 1 or epoch == epochs:
+        run_validation = (epoch % val_every == 0 or epoch == 1 or epoch == epochs)
+        model.loss.kld_weight = kld_scheduler(kld_weight, kld_exp, epoch)
+        train_one_epoch(model, dl_train, optimizer, current_epoch=epoch, total_epochs=epochs, device=device, wandb_run=wandb_run, enable_plot=run_validation)
+        if run_validation:
             validate(model, dl_valid, current_epoch=epoch, total_epochs=epochs, device=device, wandb_run=wandb_run)
             store_checkpoint(model, optimizer, current_epoch=epoch)
             
 
-def train_one_epoch(model, dl_train, optimizer, current_epoch, total_epochs, device='cpu', wandb_run=None):
+def train_one_epoch(model, dl_train, optimizer, current_epoch, total_epochs, device='cpu', wandb_run=None, enable_plot=False):
     model.train()
     # For each batch
     step = 1
+    all_labels, all_mu = [], []
     epoch_loss, epoch_loss_rec, epoch_loss_kld, epoch_loss_ce = 0, 0, 0, 0
-    for x, label in tqdm(dl_train, desc=f"Training epoch {current_epoch}/{total_epochs}"):
-        label = label.to(device)
+    for x, (labels, onehot) in tqdm(dl_train, desc=f"Training epoch {current_epoch}/{total_epochs}"):
+        onehot = onehot.float().to(device)
         x = x.to(device)
-        x_reconst, mu, logvar, z, losses = model(x, label=label)
+        x_reconst, mu, logvar, z, losses = model(x, label=onehot)
         loss = losses[0]
         epoch_loss += loss.detach().cpu().numpy()
         epoch_loss_rec += losses[1].detach().cpu().numpy()
@@ -106,6 +117,8 @@ def train_one_epoch(model, dl_train, optimizer, current_epoch, total_epochs, dev
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        all_labels.append(labels)
+        all_mu.append(mu)
         step += 1
     # Get metrics
     metrics = dict()
@@ -114,6 +127,14 @@ def train_one_epoch(model, dl_train, optimizer, current_epoch, total_epochs, dev
     metrics[f"train/avg_loss_rec"] = epoch_loss_rec / dl_len
     metrics[f"train/avg_loss_kld"] = epoch_loss_kld / dl_len
     metrics[f"train/avg_loss_ce"] = epoch_loss_ce / dl_len
+    if enable_plot:
+        all_labels = pd.concat([pd.DataFrame(l) for l in all_labels])
+        all_mu = torch.vstack(all_mu)
+        latentspace_figure_path = plot_latentspace(all_mu.detach().cpu().numpy(), all_labels, current_epoch=current_epoch, n_iter=2500)
+        latentspace_pca_figure_path, explvariance_pca_figure_path = plot_latentspace_pca(all_mu.detach().cpu().numpy(), all_labels, current_epoch=current_epoch)
+        metrics["train/latent_space"] = wandb.Image(latentspace_figure_path)
+        metrics["train/latent_space_pca"] = wandb.Image(latentspace_pca_figure_path)
+        metrics["train/expl_variance_pca"] = wandb.Image(explvariance_pca_figure_path)
     print('Training metrics: \n', pd.Series(metrics))
     if wandb_run is not None:
         wandb_run.log(metrics, step=current_epoch)
@@ -141,6 +162,7 @@ def validate(model, dl_valid, current_epoch, total_epochs, device='cpu', wandb_r
     all_labels = pd.concat([pd.DataFrame(l) for l in all_labels])
     all_mu = torch.vstack(all_mu)
     latentspace_figure_path = plot_latentspace(all_mu.detach().cpu().numpy(), all_labels, current_epoch=current_epoch)
+    latentspace_pca_figure_path, explvariance_pca_figure_path = plot_latentspace_pca(all_mu.detach().cpu().numpy(), all_labels, current_epoch=current_epoch)
     # Get metrics and return them
     metrics = dict()
     dl_len = len(dl_valid)
@@ -150,6 +172,8 @@ def validate(model, dl_valid, current_epoch, total_epochs, device='cpu', wandb_r
     metrics["valid/avg_loss_ce"] = epoch_loss_ce / dl_len
     metrics["valid/reconstructions"] = wandb.Image(reconstructrions_figure_path)
     metrics["valid/latent_space"] = wandb.Image(latentspace_figure_path)
+    metrics["valid/latent_space_pca"] = wandb.Image(latentspace_pca_figure_path)
+    metrics["valid/expl_variance_pca"] = wandb.Image(explvariance_pca_figure_path)
     print('Validation metrics: \n', pd.Series(metrics))
     if wandb_run is not None:
         wandb_run.log(metrics, step=current_epoch)
@@ -177,6 +201,7 @@ def test(model, dl_test, device='cpu', wandb_run=None):
     all_labels = pd.concat([pd.DataFrame(l) for l in all_labels])
     all_mu = torch.vstack(all_mu)
     latentspace_figure_path = plot_latentspace(all_mu.detach().cpu().numpy(), all_labels, current_epoch=0)
+    latentspace_pca_figure_path, explvariance_pca_figure_path = plot_latentspace_pca(all_mu.detach().cpu().numpy(), all_labels, current_epoch=0)
     # Get metrics and return them
     metrics = dict()
     dl_len = len(dl_test)
@@ -186,36 +211,40 @@ def test(model, dl_test, device='cpu', wandb_run=None):
     metrics["test/avg_loss_ce"] = epoch_loss_ce / dl_len
     metrics["test/reconstructions"] = wandb.Image(reconstructrions_figure_path)
     metrics["test/latent_space"] = wandb.Image(latentspace_figure_path)
+    metrics["test/latent_space_pca"] = wandb.Image(latentspace_pca_figure_path)
+    metrics["test/expl_variance_pca"] = wandb.Image(explvariance_pca_figure_path)
     print('Testing metrics: \n', pd.Series(metrics))
     if wandb_run is not None:
         wandb_run.log(metrics)
 
 
+cfg = {
+    # general
+    'seed': 42,
+    # data
+    'sr': 16000,
+    'duration': 4,
+    'batch_size': 32,
+    'num_workers': 4,
+    'n_mels': 80,
+    'data_mean': -41.5759,  # in db
+    'data_std': 38.5646,  # in db
+    # training
+    'start_lr': 1e-4,
+    'epochs': 100,
+    'val_every': 10,
+    'loss_type': 'classes',
+    # model
+    'fc_hidden1': 512,
+    'fc_hidden2': 1024,
+    'fc_hidden3': 896,
+    'lspace_size': 256,
+    # loss
+    'rec_weight': 1.0,
+    'kld_weight': 0.01,
+    'ce_weight': 0.05,
+    'kld_exp': 1.2,
+}
 
 if __name__ == "__main__":
-    cfg = Bunch({
-        # general
-        'seed': 42,
-        # data
-        'sr': 16000,
-        'duration': 4,
-        'batch_size': 32,
-        'num_workers': 4,
-        'n_mels': 80,
-        'data_mean': -41.5759,  # in db
-        'data_std': 38.5646,  # in db
-        # training
-        'start_lr': 1e-4,
-        'epochs': 4,
-        'val_every': 2,
-        # model
-        'fc_hidden1': 512,
-        'fc_hidden2': 1024,
-        'fc_hidden3': 4096,
-        'lspace_size': 256,
-        # loss
-        'rec_weight': 1.0,
-        'kld_weight': 0.01,
-        'ce_weigth': 1.0,
-    })
-    train(cfg)
+    train(Bunch(cfg))

src/utils.py
@@ -1,4 +1,3 @@
-from cProfile import label
 import os
 import random
 import datetime
@@ -11,11 +10,33 @@ from torch.utils.data import DataLoader
 from torchaudio.transforms import MelSpectrogram, AmplitudeToDB
 from torchvision.transforms import Normalize, Compose
 from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
 
 
 class Bunch(object):
     def __init__(self, adict):
         self.__dict__.update(adict)
+
+
+class SpecNormalize(torch.nn.Module):
+    def __init__(self, data_mean, data_std):
+        super(SpecNormalize, self).__init__()
+        self.data_mean = data_mean
+        self.data_std = data_std
+    
+    def forward(self, x):
+        return (x - self.data_mean) / self.data_std
+
+
+class SpecDenormalize(torch.nn.Module):
+    def __init__(self, data_mean, data_std):
+        super(SpecDenormalize, self).__init__()
+        self.data_mean = data_mean
+        self.data_std = data_std
+    
+    def forward(self, x):
+        return (x * self.data_std.to(x.device)) + self.data_mean.to(x.device)
+
 
 
 def set_seed(seed):
@@ -34,12 +55,14 @@ def get_preprocessing(train, sr=16000, n_fft=1024, win_length=1024, hop_length=2
     preprocessings = [MelSpectrogram(sample_rate=sr, n_fft=n_fft, win_length=win_length, hop_length=hop_length, n_mels=n_mels)]
     if log_transform:
         preprocessings += [AmplitudeToDB()]
-    if data_mean is not None:
+    if isinstance(data_mean, torch.Tensor):
+        preprocessings += [SpecNormalize(data_mean, data_std)]
+    elif data_mean is not None:
         preprocessings += [Normalize(data_mean, data_std)]
     return Compose(preprocessings)
 
 
-def get_dataloader(dataset, batch_size, num_workers=0, shuffle=False, seed=0):
+def get_dataloader(dataset, batch_size, num_workers=0, shuffle=False, seed=0, drop_last=True):
     generator = torch.Generator()
     generator.manual_seed(seed)
     return DataLoader(
@@ -50,7 +73,7 @@ def get_dataloader(dataset, batch_size, num_workers=0, shuffle=False, seed=0):
         pin_memory=True,
         generator=generator,
         persistent_workers=num_workers > 0,
-        drop_last=True,
+        drop_last=drop_last,
     )
 
 
@@ -86,6 +109,11 @@ def store_checkpoint(model, optimizer, current_epoch):
 
 
 def plot_reconstructions(x_true, x_reconst, current_epoch, sr=16000):
+    data_mean = torch.load('data_mean.pt')
+    data_std = torch.load('data_std.pt')
+    denorm = SpecDenormalize(data_mean=data_mean, data_std=data_std)
+    x_true = denorm(x_true)
+    x_reconst = denorm(x_reconst)
     batch_size = x_true.shape[0]
     nrows = 4
     ncols = batch_size // nrows
@@ -101,14 +129,47 @@ def plot_reconstructions(x_true, x_reconst, current_epoch, sr=16000):
     return figure_path
 
 
-def plot_latentspace(mu, labels, current_epoch):
-    fig, ax = plt.subplots(1, 1, figsize=(16, 16))
-    mu_embs = TSNE(n_components=2, n_iter=5000).fit_transform(mu)
+def plot_latentspace(mu, labels, current_epoch, n_iter=10000):
+    fig, ax = plt.subplots(1, 1, figsize=(16, 8))
+    mu_embs = TSNE(n_components=2, n_iter=n_iter, learning_rate='auto', init='pca').fit_transform(mu)
     labels['emb_1'] = mu_embs[:, 0]
     labels['emb_2'] = mu_embs[:, 1]
-    sns.scatterplot(x='emb_1', y='emb_2', hue='instrument_family_str', size='velocity', style='instrument_source_str', data=labels, ax=ax)
+    sns.scatterplot(x='emb_1', y='emb_2', hue='instrument', style='instrument', size='velocity', data=labels, ax=ax)
     fig.tight_layout()
     figure_path = os.path.join('figures', f'latentspace_epoch_{current_epoch:04}.png')
     plt.savefig(figure_path)
     plt.close(fig)
     return figure_path
+
+def plot_latentspace_pca(mu, labels, current_epoch):
+    nc = min(100, mu.shape[0])
+    pca = PCA(n_components=nc)
+    mu_embs = pca.fit_transform(mu)
+    fig, axs = plt.subplots(2, 2, figsize=(16, 8))
+    for ax, pc1, pc2 in zip(axs.flatten(), mu_embs.T[::2], mu_embs.T[1::2]):
+        labels['emb_1'] = pc1
+        labels['emb_2'] = pc2
+        sns.scatterplot(x='emb_1', y='emb_2', hue='instrument', style='instrument', size='velocity', data=labels, ax=ax)
+        ax.set_xlabel('')
+        ax.set_ylabel('')
+        ax.legend().set_visible(False)
+    handles, labels = axs[-1,-1].get_legend_handles_labels()
+    fig.legend(handles, labels, loc = 'lower center', bbox_to_anchor = (0,-0,1,1), bbox_transform = fig.transFigure, ncol=8)
+    fig.tight_layout()
+    ls_figure_path = os.path.join('figures', f'latentspace_pca_epoch_{current_epoch:04}.png')
+    plt.savefig(ls_figure_path)
+    plt.close(fig)
+
+    fig, ax = plt.subplots(1, 1, figsize=(16, 8))
+    ax.plot(pca.explained_variance_ratio_)
+    ax.set_xlim([0, nc])
+    ax.set_ylim([0, 0.5])
+    fig.tight_layout()
+    ev_figure_path = os.path.join('figures', f'explvariance_pca_epoch_{current_epoch:04}.png')
+    plt.savefig(ev_figure_path)
+    plt.close(fig)
+    return ls_figure_path, ev_figure_path
+
+
+def kld_scheduler(kld_weight, kld_exp, current_epoch):
+    return (kld_weight * kld_weight) * (current_epoch ** kld_exp)